7e397713b0bbb9b4c02af224ae195a25f4a09ca4
Taylor Thompson <jamestthompson3@gmail.com>
2020-09-30 15:13:59 +0300

    Styling changes

--- src/render/app/views/Card.jsx
+++ src/render/app/views/Card.jsx
@@ -14,14 +14,14 @@ const { WORKSPACE_REMOVE_CARD, DELETE_CARD } = MESSAGES;
 // Drag and drop all the things!!
 // very naive first pass
 function parseTemplateFields(displayFields, labelFields, contents, send) {
-  const renderFields = (fields) =>
-    fields.map((field) => (
+  const renderFields = fields =>
+    fields.map(field => (
       <div className="card-field" key={field}>
         {labelFields && <p>{field}: </p>}
         <Editable
           className="field-content"
           value={contents[field]}
-          send={(data) =>
+          send={data =>
             send({ type: "UPDATE_FIELD", data: { field, value: data } })
           }
         />
@@ -29,7 +29,7 @@ function parseTemplateFields(displayFields, labelFields, contents, send) {
     ));
   if (displayFields === "all") {
     const fields = Object.keys(contents).filter(
-      (key) => !STATIC_FIELDS.includes(key)
+      key => !STATIC_FIELDS.includes(key)
     );
     return renderFields(fields);
   }
@@ -45,20 +45,24 @@ function parseTemplate({ contents, template, send }) {
     <div className="card">
       <div className="card-title">
         <h2 data-field="title">{contents.title}</h2>
-        <button
-          onClick={() => {
-            workspaceDriver.send(WORKSPACE_REMOVE_CARD, contents.id);
-          }}
-        >
-          close
-        </button>
-        {contents.id !== "settings" && (
+        <div style={{ display: "flex" }}>
           <button
-            onClick={() => deleteCardDriver.send(DELETE_CARD, contents.id)}
+            className="action-button"
+            onClick={() => {
+              workspaceDriver.send(WORKSPACE_REMOVE_CARD, contents.id);
+            }}
           >
-            delete
+            close
           </button>
-        )}
+          {contents.id !== "settings" && (
+            <button
+              className="action-button danger"
+              onClick={() => deleteCardDriver.send(DELETE_CARD, contents.id)}
+            >
+              delete
+            </button>
+          )}
+        </div>
       </div>
       <div className="card-meta">
         <i>{contents.modifier === "" ? "Signalman User" : contents.modifier}</i>
