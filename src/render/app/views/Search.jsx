0426779befc9996e8c2586c7b068ee01f624bc48
Taylor Thompson <taylor.thompson@epicgames.com>
2020-10-21 12:19:20 +0300

    Allows for "enter" to add search result

--- src/render/app/views/Search.jsx
+++ src/render/app/views/Search.jsx
@@ -8,8 +8,12 @@ const { WORKSPACE_SEARCH, CLEAR_SEARCH, ADD_CARD } = MESSAGES;
 import { searchDriver, workspaceDriver } from "../utils/eventMachines";
 
 const searchService = searchDriver.init();
-const getCardId = (text) =>
-  text.split(window.pathSep).pop().split(".json").shift();
+const getCardId = text =>
+  text
+    .split(window.pathSep)
+    .pop()
+    .split(".json")
+    .shift();
 
 export function Search() {
   const [currentState] = useService(searchService);
@@ -17,36 +21,44 @@ export function Search() {
   React.useEffect(() => {
     return () => searchDriver.stop();
   }, []);
+  const {
+    context: { result }
+  } = currentState;
   const [focused, setFocused] = React.useState(0);
-  const captureFocus = (e) => {
+  const captureFocus = e => {
     switch (e.which) {
+      // up arrow
       case 40: {
-        if (focused < currentState.context.result.length) {
+        if (focused < result.length) {
           setFocused(focused + 1);
         }
         break;
       }
+      // down arrow
       case 38: {
         if (focused !== 0) {
           setFocused(focused - 1);
         }
         break;
       }
+      // enter
+      case 13: {
+        workspaceDriver.send(ADD_CARD, get(result, `${focused}.path.text`));
+        searchDriver.send(CLEAR_SEARCH);
+        break;
+      }
       default:
         break;
     }
   };
-  console.log(currentState.context.result);
+  console.log(result);
   return (
     <>
       <form
         className="search-form"
         ref={form}
         onSubmit={() => {
-          workspaceDriver.send(
-            ADD_CARD,
-            get(currentState, `context.result[${focused}].id`)
-          );
+          workspaceDriver.send(ADD_CARD, get(result, `${focused}.id`));
         }}
       >
         <label style={{ display: "none" }} htmlFor="search" aria-hidden="true">
@@ -58,7 +70,7 @@ export function Search() {
           className="searchbox"
           placeholder="search"
           onKeyDown={captureFocus}
-          onChange={(e) => {
+          onChange={e => {
             if (Boolean(e.target.value)) {
               searchDriver.send(WORKSPACE_SEARCH, e.target.value);
             } else {
